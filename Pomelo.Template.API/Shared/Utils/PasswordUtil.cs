d8ad1491ec76dc7be9c0bf03af0e17388b993954
Nicolas Conte <contenicolascv@gmail.com>
2024-12-28 19:59:40 -0300

    feat: implement password hashing

--- Pomelo.Template.API/Shared/Utils/PasswordUtil.cs
+++ Pomelo.Template.API/Shared/Utils/PasswordUtil.cs
@@ -1,9 +1,20 @@
-﻿namespace Pomelo.Template.API.Shared.Utils;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace Pomelo.Template.API.Shared.Utils;
 
 public class PasswordUtil
 {
     public static string HashPassword(string password)
     {
-        throw new NotImplementedException();
+        byte[] salt = [128 / 8];
+        string pwdHashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8
+        ));
+
+        return pwdHashed;
     }
 }
